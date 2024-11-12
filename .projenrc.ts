c8f6420bd729e7aaafbd9ccc61570e3c6691a968
Jayson Rawlins <JaysonJ.Rawlins@gmail.com>
2024-11-11 23:59:05 -0500

    Adding projen and CDK

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,10 @@
 import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.166.0',
   defaultReleaseBranch: 'main',
   name: 'lua-zombie-topdown-shooter',
   projenrcTs: true,
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  depsUpgrade: false,
 });
+
 project.synth();
